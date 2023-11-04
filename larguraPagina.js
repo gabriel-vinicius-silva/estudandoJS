bfc20ebe90250774791fa03b39b4e0f8324834fd
unknown <gabrieuvinici@gmail.com>
2023-11-04 11:30:06 -0300

    sugundo commit

---
+++ larguraPagina.js
@@ -0,0 +1,15 @@
+/*
+@author Gabriel Vinicius Da Silva
+*/
+
+//variaveis; 
+
+var largura = window.innerWidth ;
+//defini a largura da pagina
+alert(
+ "a largura da pagina é "+ largura +"px"  
+)
+
+//estrutura basica
+
+//back-end
