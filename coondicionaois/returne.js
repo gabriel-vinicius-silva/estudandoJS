8bd44050e21ca7bd1ccf49561ae37ac21de4829c
unknown <gabrieuvinici@gmail.com>
2023-11-08 15:22:23 -0300

    ficou lindo

--- coondicionaois/returne.js
+++ coondicionaois/returne.js
@@ -6,12 +6,3 @@
 
 vendo na pratica
 */
-function perginta() {
-    var pergunta = prompt("hoje chove ?);
-
-    if (pergunta !== 'sim') {
-        alert("iremos ir a praia");
-        return;
-    } else {
-        alert("iremos ao shopping");
-    }
