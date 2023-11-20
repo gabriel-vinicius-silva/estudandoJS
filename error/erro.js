3830b2e43cfca9f37747ddc9adfacbce201729b9
unknown <gabrieuvinici@gmail.com>
2023-11-20 19:20:11 -0300

    mais aulas

---
+++ error/erro.js
@@ -0,0 +1,21 @@
+//try significa tente.
+
+
+
+try{
+    
+    //definimos a primeira variavel ,  b=x                  
+    
+    var x=a; 
+    var b =x ;
+    
+    alert(b)
+             
+
+       // o catch serve para se o codigo que se esta sendo testado ,esta certo se tiver errado ele fara oque esta no catch
+
+ }catch(erroNaLinha3ou4) {
+
+        alert(erroNaLinha3ou4.toString (// aparecera um alerta na tela se der errdo(
+        ))
+ }       
